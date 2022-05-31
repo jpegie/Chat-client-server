0a3b30e5e3582a0e31457b70f175a64e39cf9eef
Vladimir <56363775+jpegie@users.noreply.github.com>
2022-05-31 21:18:56 +0500

    Revert "Изменяю протокол общения клиента/сервера на TCP"

--- client-server-chat/DataClasses/Common/CurrentUsers.py
+++ client-server-chat/DataClasses/Common/CurrentUsers.py
@@ -3,7 +3,6 @@ class CurrentUsers:
     receiver_name = ''
     sender_name = 's'
 
-    def __init__(self, users=None, receiver_name=''):
-        if users:
-            self.users = users
-            self.receiver_name = receiver_name
+    def __init__(self, users, receiver_name=''):
+        self.users = users
+        self.receiver_name = receiver_name
